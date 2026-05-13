a37e887f2e7929801b17ad1007454c6b78e948e6
Infernaton <72390824+Infernaton@users.noreply.github.com>
2026-05-13 16:47:02 +0200

    v1.1
    Add enum for counter to add on card

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# mtg-card-handler v1.0
+# mtg-card-handler v1.1
 
 Typescript library that regroup models + mtg rules for cards in play + other functionnality across this project
--- src/models/Enums.ts
+++ src/models/Enums.ts
@@ -5,6 +5,66 @@ export enum DeckFormat {
 	Modern,
 }
 
+// Check about Keyword counter
+export enum CounterType {
+	//#region Counter on Players
+
+	Experience,
+	Energy,
+	Poison,
+	Rad,
+
+	//#endregion
+
+	//#region Counter on permanents
+
+	/**
+	 * Act like default counter, POWER/TOUGHNESS for creature, Loyalty for planeswalker, Defense for battle
+	 */
+	Stats,
+	/**
+	 * Will act as a marker, no value intended, just presence or absence
+	 */
+	Marker,
+	/**
+	 * For certain artifact
+	 */
+	Charge,
+	/**
+	 * Used with Saga
+	 */
+	Lore,
+	/**
+	 * Used with permanent with cumulative upkeep
+	 */
+	Age,
+	/**
+	 * If a permanent with a finality counter would go to a graveyard, exile it instead.
+	 */
+	Finality,
+	/**
+	 * If a permanent with a shield counter on it would be destroyed or take damage by an effect, a shield counter is removed from the permanent instead.
+	 */
+	Shield,
+	/**
+	 * When a permanent with a stun counter becomes untapped, a stun counter is removed from it instead.
+	 */
+	Stun,
+
+	//#endregion
+
+	/**
+	 * For all unusual counter. Need to be active on each public zone (Battlefield, Graveyard, Exile)
+	 */
+	Basic,
+
+	/**
+	 * Can be used with cards in exile and permanents
+	 * Fade counter fall into this category
+	 */
+	Time,
+}
+
 export enum Zone {
 	Graveyard,
 	Exile,
